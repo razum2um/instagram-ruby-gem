31147bdb081e356603d1da20cd8a193b13f35774
Vlad Bokov <razum2um@mail.ru>
2017-02-27 19:28:50 +0700

    instagram response enumerable

--- lib/instagram/response.rb
+++ lib/instagram/response.rb
@@ -1,21 +1,44 @@
 module Instagram
-  module Response
-    def self.create( response_hash, ratelimit_hash )
-      data = response_hash.data.dup rescue response_hash
-      data.extend( self )
-      data.instance_exec do
-        %w{pagination meta}.each do |k|
-          response_hash.public_send(k).tap do |v|
-            instance_variable_set("@#{k}", v) if v
-          end
-        end
-        @ratelimit = ::Hashie::Mash.new(ratelimit_hash)
-      end
-      data
-    end
+  class Response
+    include Enumerable
 
     attr_reader :pagination
     attr_reader :meta
     attr_reader :ratelimit
+
+    def initialize(response_hash, ratelimit_hash)
+      @response_hash = response_hash
+      @ratelimit_hash = ratelimit_hash
+    end
+
+    def each
+    end
+
+    def method_missing(method_name, *arguments, &block)
+      if method_name.to_s =~ /user_(.*)/
+        user.send($1, *arguments, &block)
+      else
+        super
+      end
+    end
+
+    def respond_to_missing?(method_name, include_private = false)
+      method_name.in?(%w(pagination meta ratelimit)) || @data.send(:respond_to_missing?, method_name, include_private)
+    end
+
+    def self.create( response_hash, ratelimit_hash )
+      new(response_hash, ratelimit_hash)
+      # data = response_hash.data.dup rescue response_hash
+      # data.extend( self )
+      # data.instance_exec do
+      #   %w{pagination meta}.each do |k|
+      #     response_hash.public_send(k).tap do |v|
+      #       instance_variable_set("@#{k}", v) if v
+      #     end
+      #   end
+      #   @ratelimit = ::Hashie::Mash.new(ratelimit_hash)
+      # end
+      # data
+    end
   end
 end
